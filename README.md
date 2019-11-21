d9750c818c25b39716d2718cc8c1a16dc1b9f580
manojdsgowda <manojdsgowda@gmail.com>
2019-11-21 09:48:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-bd5nc6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-bd5nc6)
---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  //radioItems: Array<string>;
+  radioItems = ['option1', 'option2', 'option3'];
+  model   = {option: 'option1'};
+  clickMessage = '';
+
+ // constructor() {
+ //   this.radioItems = ['option1', 'option2', 'option3'];
+ // }
+
+      onClickMe() {
+    this.clickMessage=this.model.option ;
+  }
+}
